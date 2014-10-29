dbbefd9cd327218f615477ade6cca166c9282ba6
28064212D <kenny-uchiha@hotmail.com>
2014-10-29 17:32:29 -0600

    Cmmt3

---
+++ Prueba.py
@@ -0,0 +1,14 @@
+
+def sumaDos():
+    print 10*20
+
+def division(a,b):
+    result=a/b
+    print result
+
+def main():
+    sumaDos()
+    division(10, 20)
+
+if __name__ == "__main__":
+    main()
